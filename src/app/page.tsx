ea66c0b1d890f3111de73308bbfa18f630c5ff86
Ledraa <121174019+mu6m@users.noreply.github.com>
2025-07-14 07:36:58 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -104,8 +104,8 @@ export default function Page() {
             return (
               <Card key={work.company}>
                 <CardHeader>
-                  <div className="flex items-center justify-between gap-x-2 text-base">
-                    <h3 className="inline-flex items-center justify-center gap-x-1 font-semibold leading-none">
+                  <div className="flex flex-col justify-between gap-x-2 text-base">
+                    <h3 className="inline-flex items-center justify-between gap-x-1 font-semibold leading-none">
                       <a className="hover:underline" href={work.link}>
                         {work.company}
                       </a>
