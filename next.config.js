c602ea46f7ffacbf96c0c7893fa69c06c78abb0c
ledraa <theledraa@gmail.com>
2026-04-18 23:53:25 +0300

    a

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // This tells webpack to ignore these modules on the server side
+      // as they will be provided by Cloudflare's nodejs_compat layer
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        crypto: false,
+        stream: false,
+      };
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
