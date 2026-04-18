e2c3eb621b973f46961372b18aa7e714ca05cb90
ledraa <theledraa@gmail.com>
2026-04-19 00:16:27 +0300

    update os

--- next.config.js
+++ next.config.js
@@ -2,12 +2,11 @@
 const nextConfig = {
   webpack: (config, { isServer }) => {
     if (isServer) {
-      // This tells webpack to ignore these modules on the server side
-      // as they will be provided by Cloudflare's nodejs_compat layer
       config.resolve.fallback = {
         ...config.resolve.fallback,
         fs: false,
         path: false,
+        os: false,
         crypto: false,
         stream: false,
       };
--- src/app/graphql/route.ts
+++ src/app/graphql/route.ts
@@ -7,7 +7,7 @@ import { MeResolver } from "../../apollo/resolvers";
 import { buildSchema } from "type-graphql";
 import { NextRequest } from "next/server";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
 const schema = await buildSchema({
   resolvers: [MeResolver],
