e08e0065749cf222b2e41b1bf98abdc12ebae716
Ledraa <121174019+mu6m@users.noreply.github.com>
2026-06-08 01:24:15 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -173,7 +173,7 @@ export default function Page() {
           </div>
         </Section>
 
-        <Section className="print-force-new-page scroll-mb-16">
+        {/*<Section className="print-force-new-page scroll-mb-16">
           <h2 className="text-xl font-bold">Projects</h2>
           <div className="-mx-3 grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 print:grid-cols-3 print:gap-2">
             {RESUME_DATA.projects.map((project) => {
@@ -188,7 +188,7 @@ export default function Page() {
               );
             })}
           </div>
-        </Section>
+        </Section>*/}
       </section>
 
       <CommandMenu
